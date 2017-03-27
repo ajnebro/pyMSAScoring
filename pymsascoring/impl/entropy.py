8216eef2342f2bfa676cd317b8cf84727ba2d0f0
Pablo Rodríguez <Pablo Rodrguez>
2017-03-27 14:31:41 +0200

    2nd Commit PABLO

--- pymsascoring/impl/entropy.py
+++ pymsascoring/impl/entropy.py
@@ -12,11 +12,11 @@ class Entropy (Score):
         self.list=l
     
     def calculate_minimum_entropy(self):
-        sum = 0
-        maxCol = 0
-        total_seqs = len(list)
-        curr_chars = []
-        frequencies = []
+        sum = 0                     #RETURN VALUE
+        maxCol = 0                  #MAXIMUM COLUMNS FOR SEQUENCE
+        total_seqs = len(list)      #NUMBER OF SEQUENCES TO COMPARE
+        
+        
         
         #SET MAX COLUMN LENGTH
         for item in list:
@@ -24,14 +24,18 @@ class Entropy (Score):
             if currLen > maxCol:
                 maxCol= currLen
                 
+        #CALCULATE ENTROPY FOR ALL CHARS IN SEQUENCE AND SUM TOTAL
         for i in range(maxCol):
-            curr_chars[total_seqs] = [0]*len(total_seqs)
-            j=0
+            curr_chars = []                                 #INIT CURRENT CHARACTER ARRAY
+            curr_chars[total_seqs] = [0]*len(total_seqs)    #SET CURRENT CHAR ARRAY TO ZERO
             
+            #GET ALL CHARS FOR CURRENT POSITION
+            j=0
             for item in list:
                 curr_chars[j]=item[1][i]
                 j+=1
                 
+            #CREATE DICTIONARY FOR CURRENT POSITION (KEY=CHAR, VALUE=TIMES)
             diff_chars = {}
             for j in range(total_seqs):
                 if curr_chars[j] not in diff_chars:
@@ -39,53 +43,19 @@ class Entropy (Score):
                 else:
                     diff_chars[curr_chars[j]] += 1
             
+            #CREATE FREQUENCIES FOR EACH KEY IN DICTIONARY
             k=0
+            frequencies = []
             for kv in diff_chars.items():
                 frequencies[k] = kv[1]/total_seqs
                 k += 1
             
+            #CALCULATE CURRENT ENTROPY
             current_entropy = 0
             for char in len(frequencies):
                 current_entropy += frequencies[char]*math.log(frequencies[char])
             
+            #ADD CURRENT ENTROPY TO SUM
             sum+=current_entropy
         
-        """
-        def percentage_of_totally_conserved_columns(self):
-        list = self.reader.read_fasta_file_as_list_of_pairs(self.fName)
-        maxCol = 0
-        conserved_columns = 0
-        
-        for item in list:
-            currLen = len(item[1])
-            if currLen > maxCol:
-                maxCol = currLen
-        
-        for i in range(maxCol):
-            isConserved = True
-            currLet = list[1][1][i]
-            
-            for item in list:
-                if item[1][i] != currLet:
-                    isConserved = False
-                    
-            if isConserved == True:
-                conserved_columns+=1    
-                
-        return (conserved_columns/maxCol)*100
-        
-    def percentage_of_non_gaps(self):
-        list = self.reader.read_fasta_file_as_list_of_pairs(self.fName)
-        totChars = 0
-        gapCounter = 0
-        
-        for item in list:
-            currChars = len(item[1])
-            totChars += currChars
-
-            for i in range(currChars):
-                if item[1][i] == '-':
-                    gapCounter += 1
-                    
-        return 100 - (gapCounter/totChars)*100
-        """
+        return sum
