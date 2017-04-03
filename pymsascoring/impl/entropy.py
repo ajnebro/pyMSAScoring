b6aa4787c743d64b8568da23174ee47eb63e2216
Guillermo López García <guiller.lopez.garcia@gmail.com>
2017-04-03 12:09:39 +0200

    Refined entropy

--- pymsascoring/impl/entropy.py
+++ pymsascoring/impl/entropy.py
@@ -33,43 +33,31 @@ class Entropy (Score):
         self.list=l
     
     def compute(self):
-        sum = 0                         #RETURN VALUE
-        maxCol = 0                      #MAXIMUM COLUMNS FOR SEQUENCE
-        total_seqs = len(self.list)     #NUMBER OF SEQUENCES TO COMPARE
+        sum = 0                                                 #RETURN VALUE
+        n_cols = len(self.list[0][1])                           #NUMBER OF CHARACTERS OF EVERY SEQUENCE
+        total_seqs = len(self.list)                             #NUMBER OF SEQUENCES TO COMPARE
         
-        # SET MAX COLUMN LENGTH
-        for item in self.list:
-            currLen = len(item[1])
-            if currLen > maxCol:
-                maxCol= currLen
-                
         # CALCULATE ENTROPY FOR ALL CHARS IN SEQUENCE AND SUM TOTAL
-        for i in range(maxCol):
-            curr_chars = [0] * total_seqs                   #SET CURRENT CHAR LIST TO ZERO
+        for i in range(n_cols):
+            curr_chars = [0] * total_seqs                       #SET CURRENT CHAR LIST TO ZERO
             
             ### GET ALL CHARS FOR CURRENT POSITION
-            j=0                                             #SEQUENCE COUNTER FOR CURRENT CHAR ARRAY
+            j=0                                                 #SEQUENCE COUNTER FOR CURRENT CHAR ARRAY
+            ### CREATE DICTIONARY FOR CURRENT POSITION (KEY=CHAR, VALUE=RELATIVE FREQUENCY)
+            diff_chars = {}                                     # INIT DICTIONARY
             for item in self.list:
-                curr_chars[j]=item[1][i]                    #SET CURRENT CHAR FOR SEQUENCE
-                j+=1
-                
-            ### CREATE DICTIONARY FOR CURRENT POSITION (KEY=CHAR, VALUE=ABSOLUTE FREQUENCY)
-            diff_chars = {}                                 #INIT DICTIONARY
-            for j in range(total_seqs):
-                if curr_chars[j] not in diff_chars:         #IF CHAR IS NOT IN DICT, CREATE KEY
-                    diff_chars[curr_chars[j]] = 1
+                curr_chars[j]=item[1][i]                        #SET CURRENT CHAR FOR SEQUENCE
+                if curr_chars[j] not in diff_chars:             #IF CHAR IS NOT IN DICT, CREATE KEY
+                    diff_chars[curr_chars[j]] = 1/total_seqs
                 else:
-                    diff_chars[curr_chars[j]] += 1          #ELSE ADD 1 TO VALUE FOR KEY
-            
-            ### CREATE (RELATIVE) FREQUENCIES FOR EACH KEY IN DICTIONARY
-            frequencies = []                                #INIT (RELATIVE) FREQUENCY LIST
-            for kv in diff_chars.items():
-                frequencies.append(kv[1]/total_seqs)        #GET (ABSOLUTE) FREQUENCY FOR KEY
+                    diff_chars[curr_chars[j]] += 1/total_seqs   #ELSE ADD 1/TOTAL_SEQS TO VALUE FOR KEY
+                j+=1
             
             ### CALCULATE CURRENT COLUMN ENTROPY
             current_entropy = 0
-            for f in frequencies:
+            for k in diff_chars.keys():
                 #CALCULATE ENTROPY FOR CURRENT CHARACTER
+                f = diff_chars[k]
                 current_entropy += f*math.log(f)
             
             #ADD CURRENT COLUMN ENTROPY TO SUM
