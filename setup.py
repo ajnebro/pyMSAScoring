ed2273b69e18077dc1de66dc468a787bb8469615
Maria José Muñoz <Maria Jos Muoz>
2017-04-05 09:35:34 +0200

    Setup Modified

---
+++ setup.py
@@ -0,0 +1,63 @@
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pyMSAScoring',
+
+    version='1.0-SNAPSHOT',
+
+    description='A Scoring Multiple Sequence Alignments with Python',
+    long_description=long_description,
+
+    url='https://github.com/ajnebro/pyMSAScoring',
+
+    author='Antonio Jseus Nebro',
+    author_email='ajnebro@uma.es',
+    author0='Maria Jose Muñoz',
+    author_email0='marmungon@uma.es',
+    author1='Daniel Torres',
+    author_email1='dantorram@uma.es',
+    author2='Miguel Angel Gallardo',
+    author_email2='miggalrui@uma.es',
+    author3='Antonio Benitez',
+    author_email3='antonio.b@uma.es',
+    author4='Rene Betancor',
+    author_email4='',
+    author5='Juan Ignacio Alvarez',
+    author_email5='juaalvare@uma.es',
+    author6='Guillermo Lopez',
+    author_email6='guilopgar@uma.es',
+    author7='Pablo Rodriguez',
+    author_email7='pabrod@uma.es',
+
+    license='GNU',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience:: Science / Research',
+
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+
+    # What does your project relate to?
+    keywords='sample setuptools development',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+)
