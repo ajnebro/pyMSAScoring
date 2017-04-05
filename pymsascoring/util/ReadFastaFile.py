6c66ef10648a5dc6927fd96f9173bcfec89b1c2b
Maria José Muñoz <Maria Jos Muoz>
2017-04-05 09:55:21 +0200

    ReadFastaFile

---
+++ pymsascoring/util/ReadFastaFile.py
@@ -0,0 +1,28 @@
+__author__ = "Maria Jose Muñoz"
+__copyright__ = ""
+__credits__ = ["Maria Jose Muñoz"]
+__license__ = "GNU"
+__version__ = "1.0"
+__status__ = "Development"
+__email__ = ["marmungon@uma.es"]
+
+class ReadFastaFile():
+    """
+    Read a Fasta file and return a list of pairs --> [(key, value), (key, value)]
+    """
+    def read_fasta_file_as_list_of_pairs(fileName):
+        vector = []
+        key = ''
+        value = ''
+        with open(fileName, 'r') as file:
+            for line in file:
+                if line[0] == '>':
+                    if key != '':
+                        vector.append((key, value))
+                    key = line[1:].rstrip()
+                    value = ''  # aun no hemos leido ninguna secuencia
+                else:
+                    value += line.rstrip()
+        vector.append((key, value))
+        file.close()
+        return vector
