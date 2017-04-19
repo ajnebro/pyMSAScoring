260a8b47e57e60667f93d611b0e98db455149c57
Antonio Benitez <antbenhid@uma.es>
2017-04-19 11:48:55 +0200

    Added percentage of non-gaps

--- pymsascoring/examples/scoringExample.py
+++ pymsascoring/examples/scoringExample.py
@@ -3,6 +3,7 @@ from pymsascoring.substitutionmatrix.impl.pam250 import PAM250
 from pymsascoring.score.impl.entropy import Entropy
 from pymsascoring.score.impl.sumofpairs import SumOfPairs
 from pymsascoring.score.score import Score
+from pymsascoring.score.impl.percentageofnongaps import PercentageOfNonGaps
 
 __author__ = "Antonio J. Nebro"
 __license__ = "GPL"
@@ -13,6 +14,7 @@ __email__ = "antonio@lcc.uma.es"
 """
 This program is intended to show some examples of using the scores in pyMSA.
 """
+
 
 def compute_score(score : Score, msa) -> float:
     """
@@ -26,14 +28,18 @@ def compute_score(score : Score, msa) -> float:
 if __name__ == '__main__':
     msa = (("s1", "ACTG"), ("S2", "A-T-"))
 
+    pong = PercentageOfNonGaps(msa)
+    percentage = pong.percentage_of_non_gaps()
+    print("Percentage of non-gaps: {0} %".format(percentage))
+
     score_method = Entropy()
     value = compute_score(score_method, msa)
-    print(value)
+    print("Entropy score: {0}".format(value))
 
     score_method = SumOfPairs(Blosum62())
     value = compute_score(score_method, msa)
-    print(value)
+    print("SumOfPairs score (Blosum62): {0}".format(value))
 
     score_method = SumOfPairs(PAM250())
     value = compute_score(score_method, msa)
-    print(value)
+    print("SumOfPairs score (PAM250): {0}".format(value))
