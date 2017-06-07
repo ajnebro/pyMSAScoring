d0cc58fe52de6702c37c0cfa021493df10465f48
ajnebro <antonio@lcc.uma.es>
2017-06-07 09:49:18 +0200

    Modify file setup.py

--- setup.py
+++ setup.py
@@ -3,19 +3,12 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
     name='pyMSAScoring',
 
-    version='0.1',
+    version='0.1.1',
 
     description='Scoring Multiple Sequence Alignments with Python',
-    long_description=long_description,
 
     url='https://github.com/ajnebro/pyMSAScoring',
 
