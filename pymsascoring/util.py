1d8e0000126a59b9464405cb5ce2027385d50655
Qallidal <pabrod@uma.es>
2017-06-18 22:23:55 +0200

    Read FASTQ

--- pymsascoring/util.py
+++ pymsascoring/util.py
@@ -1,3 +1,6 @@
+from itertools import islice
+
+
 class ReadFastaFile():
     """
     Read a Fasta file and return a list of pairs --> [(key, value), (key, value)]
@@ -25,3 +28,27 @@ class ReadFastaFile():
 
             for (pair1, pair2) in list:
                 output.write(pair1 + ',' + pair2 + '\n')
+
+
+class ReadFastqFile():
+    """
+    Read a FASTQ file and return a list of pairs -->  [(key, value), (key, value)]
+    """
+    def read_fastq_file_as_list_of_pairs(filename):
+        vector = []
+        key = ''
+        value = ''
+        groups = []
+        with open(filename, 'r') as file:
+            for line in file:
+                if len(groups) == 4:
+                    key = groups[0][1:].rstrip()
+                    value = groups[1].rstrip()
+                    groups = []
+                    vector.append((key, value))
+                groups.append(line)
+        key = groups[0][1:].rstrip()
+        value = groups[1].rstrip()
+        vector.append((key, value))
+        file.close()
+        return vector
