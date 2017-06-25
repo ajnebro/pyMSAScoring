861fc8ac82ff617527021fc5c7604860e8bdb095
benhid <antonio.b@uma.es>
2017-06-26 01:26:01 +0200

    Deleted logger

--- pymsascoring/test/test_scoring.py
+++ pymsascoring/test/test_scoring.py
@@ -1,16 +1,12 @@
-import logging
 import unittest
 
 from pymsascoring.score import SumOfPairs, Star, PercentageOfTotallyConservedColumns, Entropy
 from pymsascoring.substitutionmatrix import PAM250, Blosum62
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 
 class SumOfPairsTestCases(unittest.TestCase):
     def setUp(self):
-        print("setUp: RUNNING TEST")
+        print("setUp: RUNNING SumOfPairsTestCases")
         self.sumofpairs_PAM250 = SumOfPairs(PAM250())
         self.sumofpairs_Blosum62 = SumOfPairs(Blosum62())
 
@@ -18,8 +14,6 @@ class SumOfPairsTestCases(unittest.TestCase):
         print("tearDown: TEST ENDED")
 
     def test_basic_score_of_12_with_PAM250(self):
-        logger.info("Test for compute()...")
-
         # setup
         msa = [('ID1', 'AA'), ('ID2', 'AA'), ('ID3', 'AA')]
 
@@ -31,8 +25,6 @@ class SumOfPairsTestCases(unittest.TestCase):
         self.assertEqual(expected, result)
 
     def test_basic_score_of_12_with_BLOSUM62(self):
-        logger.info("Test for compute()...")
-
         # setup
         msa = [('ID1', 'AA'), ('ID2', 'AA'), ('ID3', 'AA')]
 
@@ -44,8 +36,6 @@ class SumOfPairsTestCases(unittest.TestCase):
         self.assertEqual(expected, result)
 
     def test_basic_score_with_gaps_BLOSUM62(self):
-        logger.info("Test for compute()...")
-
         # setup
         msa = [('ID1', 'FA'), ('ID2', 'A-')]
 
@@ -57,8 +47,6 @@ class SumOfPairsTestCases(unittest.TestCase):
         self.assertEqual(expected, result)
 
     def test_only_gaps_with_BLOSUM62(self):
-        logger.info("Test for compute()...")
-
         # setup
         msa = [('ID1', '---'), ('ID2', '---')]
 
@@ -70,8 +58,6 @@ class SumOfPairsTestCases(unittest.TestCase):
         self.assertEqual(expected, result)
 
     def test_get_score_of_A_and_gap(self):
-        logger.info("Test for get_score_of_two_chars()...")
-
         # setup
         charA, charB = 'A', '-'
 
@@ -83,8 +69,6 @@ class SumOfPairsTestCases(unittest.TestCase):
         self.assertEqual(expected, result)
 
     def test_get_score_of_column_with_only_gaps(self):
-        logger.info("Test for get_score_of_k_column()...")
-
         # setup
         column = ['-', '-', '-']
 
@@ -98,7 +82,7 @@ class SumOfPairsTestCases(unittest.TestCase):
 
 class StarTestCases(unittest.TestCase):
     def setUp(self):
-        print("setUp: RUNNING TEST")
+        print("setUp: RUNNING StarTestCases")
         self.star_PAM250 = Star(PAM250())
         self.star_Blosum62 = Star(Blosum62())
 
@@ -106,8 +90,6 @@ class StarTestCases(unittest.TestCase):
         print("tearDown: TEST ENDED")
 
     def test_most_frequent_A_with_BLOSUM62(self):
-        logger.info("Test for test_most_frequent_with_BLOSUM62()...")
-
         # setup
         msa = [('sec1', 'AA'), ('sec2', 'AC'), ('sec3', 'AC')]
 
@@ -119,8 +101,6 @@ class StarTestCases(unittest.TestCase):
         self.assertEqual(expected, result)
 
     def test_most_frequent_error_with_BLOSUM62(self):
-        logger.info("Test for test_most_frequent_error_with_BLOSUM62()...")
-
         # setup
         msa = [('sec1', 'AA'), ('sec2', 'AC'), ('sec3', 'AC')]
 
@@ -133,8 +113,6 @@ class StarTestCases(unittest.TestCase):
             self.assertEqual(expected, result)
 
     def test_most_frequent_with_PAM250(self):
-        logger.info("Test for test_most_frequent_with_PAM250()...")
-
         # setup
         msa = [('sec1', 'AA'), ('sec2', 'AC'), ('sec3', 'AC')]
 
@@ -146,8 +124,6 @@ class StarTestCases(unittest.TestCase):
         self.assertEqual(expected, result)
 
     def test_most_frequent_error_with_PAM250(self):
-        logger.info("Test for test_most_frequent_error_with_PAM250()...")
-
         # setup
         msa = [('sec1', 'AA'), ('sec2', 'AC'), ('sec3', 'AC')]
 
@@ -160,8 +136,6 @@ class StarTestCases(unittest.TestCase):
             self.assertEqual(expected, result)
 
     def test_most_frequent_gaps_with_PAM250(self):
-        logger.info("Test for test_most_frequent_gaps_with_PAM250()...")
-
         # setup
         msa = [('sec1', 'AA'), ('sec2', 'A-'), ('sec3', 'AC')]
 
@@ -173,8 +147,6 @@ class StarTestCases(unittest.TestCase):
         self.assertEqual(expected, result)
 
     def test_most_frequent_gaps_with_BLOSUM62(self):
-        logger.info("Test for test_most_frequent_gaps_with_BLOSUM62()...")
-
         # setup
         msa = [('sec1', 'AA'), ('sec2', 'A-'), ('sec3', 'AC')]
 
@@ -185,38 +157,15 @@ class StarTestCases(unittest.TestCase):
         # check
         self.assertEqual(expected, result)
 
+
 class PercentageOfTotallyConservedColumnsTestCases(unittest.TestCase):
     def setUp(self):
-        print("setUp: RUNNING TEST")
+        print("setUp: RUNNING PercentageOfTotallyConservedColumnsTestCases")
 
     def tearDown(self):
         print("tearDown: TEST ENDED")
 
     def test_percentage_of_totally_conserved_columns_100(self):
-        logger.info("Test for test_percentage_of_totally_conserved_columns_100()...")
-
-        # setup
-        msa = [["S1", "AdddAAA"], ["S2", "AdddAAA"]]
-        self.per = PercentageOfTotallyConservedColumns(msa)
-
-        # results
-        result = self.per.percentage_of_totally_conserved_columns()
-        expected = 100
-
-        # check
-        self.assertEqual(result, expected)
-
-
-class PercentageOfTotallyConservedColumnsTestCases(unittest.TestCase):
-    def setUp(self):
-        print("setUp: RUNNING TEST")
-
-    def tearDown(self):
-        print("tearDown: TEST ENDED")
-
-    def test_percentage_of_totally_conserved_columns_100(self):
-        logger.info("Test for test_percentage_of_totally_conserved_columns_100()...")
-
         # setup
         msa = [["S1", "AdddAAA"], ["S2", "AdddAAA"]]
         self.per = PercentageOfTotallyConservedColumns(msa)
@@ -231,15 +180,13 @@ class PercentageOfTotallyConservedColumnsTestCases(unittest.TestCase):
 
 class EntropyTestCases(unittest.TestCase):
     def setUp(self):
-        print("setUp: RUNNING TEST")
+        print("setUp: RUNNING EntropyTestCases")
         self.ent = Entropy()
 
     def tearDown(self):
         print("tearDown: TEST ENDED")
 
     def test_get_entropy_of_a_column_with_gaps(self):
-        logger.info("Test for test_get_entropy_of_a_column_with_gaps()...")
-
         # setup
         d = {"-": 0.8, "A": 0.2}
 
@@ -251,8 +198,6 @@ class EntropyTestCases(unittest.TestCase):
         self.assertEqual(expected, result)
 
     def test_get_entropy_of_a_gapped_column(self):
-        logger.info("Test for test_get_entropy_of_a_gapped_column()...")
-
         # setup
         d = {"-": 1}
 
@@ -264,8 +209,6 @@ class EntropyTestCases(unittest.TestCase):
         self.assertEqual(expected, result)
 
     def test_get_dictionary_of_a_five_letter_column(self):
-        logger.info("Test for test_get_dictionary_of_a_five_letter_column()...")
-
         # setup
         column = ("-", "A", "C", "G", "T")
         tot_seqs = len(column)
@@ -278,8 +221,6 @@ class EntropyTestCases(unittest.TestCase):
         self.assertEqual(expected, result)
 
     def test_get_dictionary_of_a_gapped_column(self):
-        logger.info("Test for test_get_dictionary_of_a_gapped_column()...")
-
         # setup
         column = ("-", "-", "-", "-", "-")
 
@@ -291,8 +232,6 @@ class EntropyTestCases(unittest.TestCase):
         self.assertEqual(expected, result)
 
     def test_compute_of_four_seqs_with_no_gaps(self):
-        logger.info("Test for test_compute_of_four_seqs_with_no_gaps()...")
-
         # setup
         msa = [("S1", "ACGT"), ("S2", "ACGT"), ("S3", "TGCA"), ("S4", "TGCA")]
 
@@ -304,8 +243,6 @@ class EntropyTestCases(unittest.TestCase):
         self.assertEqual(expected, result)
 
     def test_compute_of_three_seqs_with_gaps(self):
-        logger.info("Test for test_compute_of_three_seqs_with_gaps()...")
-
         # setup
         msa = [("S1", "A-TGCAAT-G"), ("S2", "-CT-CCAT-A"), ("S3", "-TTAT-CTG-")]
 
@@ -317,8 +254,6 @@ class EntropyTestCases(unittest.TestCase):
         self.assertEqual(expected, result)
 
     def test_compute_of_two_gapped_seqs(self):
-        logger.info("Test for test_compute_of_two_gapped_seqs()...")
-
         # setup
         msa = [("S1", "-----"), ("S2", "-----")]
 
